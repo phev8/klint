84f60e2673a06db75966cb1ab9a0d1c143175afa
Kevin Brendel <kBrendel@hotmail.de>
2021-07-16 11:12:41 +0200

    Draw fake MarkingData box.

--- lib/ui/pages/annotation_page/components/annotation_view/components/annotations_overlay/components/annotations/annotation_painter.dart
+++ lib/ui/pages/annotation_page/components/annotation_view/components/annotations_overlay/components/annotations/annotation_painter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:klint/state/data/marking_data_state.dart';
+import 'package:provider/provider.dart';
 
 class AnnotationPainter extends CustomPainter {
   final BuildContext context;
@@ -9,6 +11,21 @@ class AnnotationPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     var rect = Offset.zero & size;
     // canvas.drawRect(rect, Paint()..color = Color.fromRGBO(0, 255, 0, 0.2));
+    var boxMarkings = context.read<MarkingDataState>().markingData?.boxMarkings;
+
+    if (boxMarkings != null) {
+      boxMarkings.forEach((boxMarking) {
+        Rect rect = Rect.fromPoints(
+            Offset(size.width / boxMarking.first[0] * 40, size.height / boxMarking.first[1] * 40),
+            Offset(size.width / boxMarking.second[0] * 40, size.height / boxMarking.second[1] * 40));
+        canvas.drawRect(
+            rect,
+            Paint()
+              ..color = Colors.red
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = 4);
+      });
+    }
   }
 
   @override
